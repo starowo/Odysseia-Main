fed48e446f04ea626ff2ab893b6f50c7a1e9b38e
Star <87891461+starowo@users.noreply.github.com>
2025-07-12 01:33:41 +0800

    fix: dm bot

--- src/utils/dm.py
+++ src/utils/dm.py
@@ -5,7 +5,7 @@ dm_bot: discord.Client = None
 async def init_dm_bot(token: str):
     # init dm bot
     global dm_bot
-    dm_bot = discord.Client(intents=discord.Intents.all())
+    dm_bot = discord.Client(intents=discord.Intents.default())
     await dm_bot.login(token)
     await dm_bot.connect()
     return dm_bot
