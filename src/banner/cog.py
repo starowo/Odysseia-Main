1b5f7b0b824f8a05174c3fd10550e94682b5caa9
starowo <87891461+starowo@users.noreply.github.com>
2025-10-21 22:52:17 -0700

    fix: banner image

--- src/banner/cog.py
+++ src/banner/cog.py
@@ -392,6 +392,8 @@ class BannerCommands(commands.Cog):
                     }
                     if 'image' in event_kwargs:
                         update_kwargs['image'] = event_kwargs['image']
+                    else:
+                        update_kwargs.pop('image', None)
                     event = await guild.fetch_scheduled_event(config.event_id)
                     await event.edit(**update_kwargs)
                     if self.logger:
